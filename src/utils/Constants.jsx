617ef8f4c666b080375985f463c8f12dfd0bca2a
faansy <159968826+faansy@users.noreply.github.com>
2024-03-08 20:15:04 +0400

    Update Constants.jsx

--- src/utils/Constants.jsx
+++ src/utils/Constants.jsx
@@ -1,8 +1,9 @@
 const Constants = {
-    clientURL: 'http://localhost:5173',
+    // clientURL: 'http://localhost:5173',
+    clientURL: 'https://faansy.com',
     // clientURL: 'https://faansy-client-production.up.railway.app',
-    serverURL: 'http://127.0.0.1:8000',
-    // serverURL: 'https://faansy-server-production.up.railway.app',
+    // serverURL: 'http://127.0.0.1:8000',
+    serverURL: 'https://faansy-server-production.up.railway.app',
 }
 
 export default Constants
